be29062552ffc998a5f1c6e46a3dc37f5b89f3fc
Vinicius Vefago <vinivefago@gmail.com>
2023-07-25 09:17:16 -0300

    users,cool,beach

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,14 @@ router.get('/', function(req, res, next) {
 
 router.get('/cool', function(req, res, next) {
   res.send('You are so cool');
-});   
+});  
+
+router.get('/cool/beach', function(req, res, next) {
+  res.send('Adoroooooooooooooooo praiaaaaaaaaaaaa, cade minhas férias!!!');
+});
+
+router.get('/ifc', function(req, res, next) {
+  res.send('Bem-vindo ao IFC!!!');
+});
 
 module.exports = router;
